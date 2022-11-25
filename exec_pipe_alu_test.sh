5055b895a18ce122d578d8bf671f4bb6a2928542
Rohan Rajesh Kalbag <46604893+rohankalbag@users.noreply.github.com>
2022-11-26 01:06:11 +0530

    Added serlok tip for bash file

--- exec_pipe_alu_test.sh
+++ exec_pipe_alu_test.sh
@@ -1,9 +1,4 @@
-ghdl -a exec_pipe_alu.vhdl
-ghdl -a alu.vhdl
-ghdl -a bit1shift.vhdl
-ghdl -a se6.vhdl
-ghdl -a exec_pipe_alu_tb.vhdl
-
+ghdl -a *.vhdl
 ghdl -e aluexecpipe
 ghdl -e tb
 ghdl -r tb --wave=waveform.ghw
