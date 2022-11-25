8ab8ea0d7830bd128b11a207fd2b0877a34763e4
Rohan Rajesh Kalbag <46604893+rohankalbag@users.noreply.github.com>
2022-11-26 01:04:02 +0530

    Added testbench and fixed minor issues of sensitivity list

--- exec_pipe_alu.vhdl
+++ exec_pipe_alu.vhdl
@@ -8,6 +8,8 @@ entity aluexecpipe is
         --  00 - opr2, 01 - leftshift(opr2), 10 - se6(opr2), 11 - invalid
         
         -- bit 3 and 2 are for alu operation same as in alu.vhdl
+        --  01 - add, 10 - nand, 11 - xor, 00 - invalid
+
         -- bit 4 is c_flag_enable for carry flag modification
         -- bit 5 is z_flag_enable for zero flag modification
         opr1, opr2, pc_in: in std_logic_vector(15 downto 0);
@@ -48,7 +50,7 @@ architecture behavioural of aluexecpipe is
 
 begin
 
-    p1: process(c_sig_in, opr1, opr2, pc_in, c_in, z_in, z_sig, c_sig)
+    p1: process(c_sig_in, opr1, opr2, pc_in, c_in, z_in, opr2_ls_sig, opr2_se_sig)
         begin
         if(c_sig_in(1 downto 0) = "00") then
             b_sig <= opr2;
@@ -61,20 +63,20 @@ begin
         end if;
     end process p1;
 
-    p2: process(c_sig_in, opr1, opr2, pc_in, c_in, z_in, z_sig, c_sig)
+    p2: process(c_sig_in, opr1, opr2, pc_in, c_in, z_in, c_sig_alu_out, z_sig_alu_out)
         begin
-        if(c_sig_in(5 downto 4) = "00") then
-            c_sig <= c_in;
-            z_sig <= z_in;
+        if(c_sig_in(5 downto 4) = "11") then
+            c_out <= c_sig_alu_out;
+            z_out <= z_sig_alu_out;
         elsif(c_sig_in(5 downto 4) = "01") then
-            c_sig <= c_in;
-            z_sig <= z_sig_alu_out;
+            c_out <= c_in;
+            z_out <= z_sig_alu_out;
         elsif(c_sig_in(5 downto 4) = "10") then
-            c_sig <= c_sig_alu_out;
-            z_sig <= z_in;
+            c_out <= c_sig_alu_out;
+            z_out <= z_in;
         else
-            c_sig <= c_sig_alu_out;
-            z_sig <= z_sig_alu_out;
+            c_out <= c_in;
+            z_out <= z_in;
         end if;
     end process p2;
 
@@ -83,6 +85,4 @@ begin
     b1: bit1shift port map(a => opr2, op => opr2_ls_sig);
     a_sig <= opr1; 
     pc_out <= pc_in;
-    c_out <= c_sig;
-    z_out <= z_sig;
 end behavioural;
--- exec_pipe_alu_test.sh
+++ exec_pipe_alu_test.sh
@@ -2,4 +2,8 @@ ghdl -a exec_pipe_alu.vhdl
 ghdl -a alu.vhdl
 ghdl -a bit1shift.vhdl
 ghdl -a se6.vhdl
+ghdl -a exec_pipe_alu_tb.vhdl
+
 ghdl -e aluexecpipe
+ghdl -e tb
+ghdl -r tb --wave=waveform.ghw
