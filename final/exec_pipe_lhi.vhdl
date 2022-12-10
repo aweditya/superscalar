71c192c657a04b0a35e0a8d4fca1d189a6e5b645
Aditya <aweditya@gmail.com>
2022-12-10 13:27:26 +0530

    Added LHI execution pipeline (tested)

---
+++ final/exec_pipe_lhi.vhdl
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity lhiexecpipe is
+    port(
+        data_in: in std_logic_vector(8 downto 0);
+        data_out: out std_logic_vector(15 downto 0)
+    );
+end entity lhiexecpipe;
+
+architecture behavioural of lhiexecpipe is
+begin
+    data_out <= data_in & "0000000"; 
+end architecture behavioural;
