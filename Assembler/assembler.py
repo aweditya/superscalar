06910e58df30aa8fac526505175e9e3e8c50e770
Rohan Rajesh Kalbag <46604893+rohankalbag@users.noreply.github.com>
2022-12-13 15:55:56 +0530

    Added fixed assembler, took instruction format into consideration, also added feature of inline and out of line comments

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -1,129 +1,142 @@
-"""
-Author: Nikhil Kaniyeri
-Description: Converts human readable ASM code to binary that can be loaded directly on to the processor memory.
-"""
-import re
-binary=""
 
-with open("code.asm", 'r') as codedata:
-    #codefull = codedata.read()
-    for code in codedata:
-        opcode,operands = code.split(" ")
-        if (opcode=="add" or opcode=="adc" or opcode=="adz" or opcode=="adl"):
-            binary+="0001"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-            binary+="0"
-            if(opcode=="add"):
-                binary+="00"
-            elif (opcode=="adc"):
-                binary+="10"
-            elif (opcode=="adz"):
-                binary+="01"
-            else:
-                binary+="11"
-        if (opcode=="adi"):
-            binary+="0000"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                else:
-                    binary+=bin(int(i[1]))[2:].zfill(6)
-        if (opcode=="ndu" or opcode=="ndc" or opcode=="ndz"):
-            binary+="0010"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-            binary+="0"
-            if(opcode=="ndu"):
-                binary+="00"
-            elif (opcode=="ndc"):
-                binary+="10"
-            elif (opcode=="ndz"):
-                binary+="01"
-        if (opcode=="lhi"):
-            binary+="0000"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                else:
-                    binary+=bin(int(i[1]))[2:].zfill(9)
-        if (opcode=="lw"):
-            binary+="0111"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                else:
-                    binary+=bin(int(i[1]))[2:].zfill(6)
-        if(opcode=="sw"):
-            binary+="0101"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                else:
-                    binary+=bin(int(i[1]))[2:].zfill(6)
-        if(opcode=="lm"):
-            binary+="1100"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                else:
-                    binary+=bin(int(i[1]))[2:].zfill(9)
-        if(opcode=="sm"):
-            binary+="1101"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                else:
-                    binary+=bin(int(i))[2:].zfill(9)
-        if(opcode=="beq"):
-            binary+="1000"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                else:
-                    binary+=bin(int(i))[2:].zfill(6)
-        if(opcode=="jal"):
-            binary+="1001"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                else:
-                    binary+=bin(int(i))[2:].zfill(9)
-        if(opcode=="jlr"):
-            binary+="1010"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                binary+="000000"                    
-        if(opcode=="jri"):
-            binary+="1011"
-            terms=operands.split(",")
-            for i in terms:
-                if i[0]=="r":
-                    binary+=bin(int(i[1]))[2:].zfill(3)
-                else:
-                    binary+=bin(int(i))[2:].zfill(9)    
-        
-        
-                
+#IITB RISC ASSEMBLER
+
+'''
+Authors: Nikhil Kaniyeri, Rohan Rajesh Kalbag
+This assembler is capable of converting IITB-RISC-22 ISA assembly instructions to binaries
+Which can be fed to the memory of our processor using a bootloader
+
+If filename.asm is to be assembled then
+Command Format: python assembler.py <filename>
+'''
+
+import sys
+binaries = ''
+
+if __name__ == '__main__':
+    params = sys.argv
+    if(len(params) > 1):
+        filename = params[1]
+        with open(filename + '.asm', 'r') as t:
+            code = t.readlines()
+            # comment handler
+            code = [i for i in code if i[0] != ';' or len(i) != 0]
             
-print (binary)                    
-            
+            for i, j in enumerate(code):
+                if ';' in j:
+                    j = j.split(';')[0]
+                    code[i] = j
+            code = [i for i in code if i != '']
+            for line in code:
+                inst, args = line.split()[:2]
+                args = args.split(',')
+                if(inst=="add" or inst=="adc" or inst=="adz" or inst=="adl"):
+                    binaries+="0001"
+                    regs = ''
+                    for i in args:
+                        if i[0] == "r":
+                            regs = bin(int(i[1]))[2:].zfill(3) + regs
+                    
+                    binaries += regs[3:6] + regs[:3] + regs[6:] # due to encoding being ra rb rc, asm being rc ra rb
         
+                    binaries += "0"
+                    if (inst == "add"):
+                        binaries += "00"
+                    elif (inst == "adc"):
+                        binaries += "10"
+                    elif (inst == "adz"):
+                        binaries += "01"
+                    else:
+                        binaries += "11"
+                if(inst == "adi"):
+                    binaries += "0000"
+                    regs = ''
+                    for i in args:
+                        if i[0] == "r":
+                            regs = bin(int(i[1]))[2:].zfill(3) + regs
+                        else:
+                            binaries += regs
+                            binaries += bin(int(i))[2:].zfill(6)
+                if(inst == "ndu" or inst == "ndc" or inst == "ndz"):
+                    binaries += "0010"
+                    regs = ''
+                    for i in args:
+                        if i[0] == "r":
+                            regs = bin(int(i[1]))[2:].zfill(3) + regs
+                    binaries += regs[3:6] + regs[:3] + regs[6:] # due to encoding being ra rb rc, asm being rc ra rb
+                    binaries += "0"
+                    if(inst == "ndu"):
+                        binaries += "00"
+                    elif (inst == "ndc"):
+                        binaries += "10"
+                    elif (inst == "ndz"):
+                        binaries += "01"
+                if(inst == "lhi"):
+                    binaries += "0011"
+                    for i in args:
+                        if i[0]=="r":
+                            binaries += bin(int(i[1]))[2:].zfill(3)
+                        else:
+                            binaries += bin(int(i))[2:].zfill(9)
+                if(inst == "lw"):
+                    binaries += "0111"
+                    for i in args:
+                        if i[0]=="r":
+                            binaries += bin(int(i[1]))[2:].zfill(3)
+                        else:
+                            binaries += bin(int(i))[2:].zfill(6)
+                if(inst == "sw"):
+                    binaries += "0101"
+                    for i in args:
+                        if i[0] == "r":
+                            binaries += bin(int(i[1]))[2:].zfill(3)
+                        else:
+                            binaries += bin(int(i))[2:].zfill(6)
+                if(inst == "lm"):
+                    binaries += "1100"
+                    for i in args:
+                        if i[0] == "r":
+                            binaries += bin(int(i[1]))[2:].zfill(3)
+                        else:
+                            binaries += bin(int(i))[2:].zfill(9)
+                if(inst == "sm"):
+                    binaries += "1101"
+                    for i in args:
+                        if i[0] == "r":
+                            binaries += bin(int(i[1]))[2:].zfill(3)
+                        else:
+                            binaries += bin(int(i))[2:].zfill(9)
+                if(inst == "beq"):
+                    binaries += "1000"
+                    for i in args:
+                        if i[0] == "r":
+                            binaries += bin(int(i[1]))[2:].zfill(3)
+                        else:
+                            binaries += bin(int(i))[2:].zfill(6)
+                if(inst == "jal"):
+                    binaries += "1001"
+                    for i in args:
+                        if i[0] == "r":
+                            binaries += bin(int(i[1]))[2:].zfill(3)
+                        else:
+                            binaries += bin(int(i))[2:].zfill(9)
+                if(inst == "jlr"):
+                    binaries += "1010"
+                    for i in args:
+                        if i[0] == "r":
+                            binaries += bin(int(i[1]))[2:].zfill(3)
+                        binaries += "000000"                    
+                if(inst == "jri"):
+                    binaries += "1011"
+                    for i in args:
+                        if i[0] == "r":
+                            binaries += bin(int(i[1]))[2:].zfill(3)
+                        else:
+                            binaries += bin(int(i))[2:].zfill(9)                  
         
-
-
+        with open('source.bin', 'w') as file:
+            file.write(binaries)
         
+        print("Assembled code successfully to /source.bin")
+    else:
+        print("No filename was passed")
